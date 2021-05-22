0c768c86c7d7196527469315e2d40f58586234e4
deadsite <84584924+deadsite@users.noreply.github.com>
2021-05-22 15:07:17 +0300

    Add files via upload

--- siteconnectivitychecker.py
+++ siteconnectivitychecker.py
@@ -1,22 +1,28 @@
 import requests
+from time import sleep
 
-f = open('result.txt', 'tw', encoding = 'utf-8')
+f = open('result_scc.txt', 'tw', encoding = 'utf-8')
 
-def SCC(url, times):
-    global f
-    i = 1
-    while i <= times:
-    	try:
-        	requests.get(url)
-        	f.write("The connection is excellent, Attempt: " + str(i) + "\n")
-        	print("Attempt:", i)
-        	i += 1
-    	except requests.ConnectionError as err:
-        	f.write("No connection, Attempt: " + str(i) + "\n")
-        	print("Attempt:", i)
-        	i += 1
+def siteconnectivitychecker(url, count, time):
+	global f
+	i = 1
+	while i <= count:
+		try:
+			requests.get(url)
+			f.write("The connection to " + url + " is excellent, Attempt: " + str(i) + "\n")
+			print("Attempt:", i)
+			if i != count:
+				sleep(time)
+			i += 1
+		except requests.ConnectionError as err:
+			f.write("No connection to " + url + ", Attempt: " + str(i) + "\n")
+			print("Attempt:", i)
+			if i != count:
+				sleep(time)
+			i += 1
 
 url = input("Enter website url: ")
-times = int(input("Enter the number of attempts: "))
-SCC(url, times)
+count = int(input("Enter the number of attempts: "))
+time = float(input("Enter the delay in seconds: "))
+siteconnectivitychecker(url, count, time)
 f.close()
