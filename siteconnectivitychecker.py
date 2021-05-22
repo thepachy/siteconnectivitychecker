0e5873e7276eb420311937f5137c7e3749baf7c9
deadsite <84584924+deadsite@users.noreply.github.com>
2021-05-22 10:23:08 +0300

    Add files via upload

--- siteconnectivitychecker.py
+++ siteconnectivitychecker.py
@@ -4,12 +4,19 @@ f = open('result.txt', 'tw', encoding = 'utf-8')
 
 def SCC(url):
     global f
-    try:
-        requests.get(url)
-        f.write("The connection is excellent")
-    except requests.ConnectionError as err:
-        f.write("No connection")    
+    i = 1
+    while i <= times:
+    	try:
+        	requests.get(url)
+        	f.write("The connection is excellent, Attempt: " + str(i) + "\n")
+        	print("Attempt:", i)
+        	i += 1
+    	except requests.ConnectionError as err:
+        	f.write("No connection, Attempt: " + str(i) + "\n")
+        	print("Attempt:", i)
+        	i += 1
 
 url = input("Enter website url: ")
+times = int(input("Enter the number of attempts: "))
 SCC(url)
 f.close()
